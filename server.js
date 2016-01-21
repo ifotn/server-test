c2981248406c12f5e5a9d1f291856158447992bd
ifotn <rich@infrontofthenet.com>
2016-01-21 16:41:53 -0500

    changed env casing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 // const hostname = '127.0.0.1';
-var port = process.ENV.port || 1337;
+var port = process.env.port || 1337;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
